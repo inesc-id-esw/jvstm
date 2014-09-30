0064805ecc8b1f4a0cbe552e9fd3b35030dd57c6
nmldiegues <nmld@ist.utl.pt>
2014-09-30 15:25:36 +0100

    Remove old scripts for parallel nesting examples

--- examples/parallel-nesting/simple-array.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm simple/*.class
-javac -cp ../../target/jvstm-1.0-SNAPSHOT.jar:. simple/SimpleVArraysParallelTest.java
-java -cp ../../target/jvstm-1.0-SNAPSHOT.jar:. simple.SimpleVArraysParallelTest
-rm simple/*.class
--- examples/parallel-nesting/simple-test-normal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm simple/*.class
-javac -cp ../../target/jvstm-1.0-SNAPSHOT.jar:. simple/SimpleParallelTest.java
-java -cp ../../target/jvstm-1.0-SNAPSHOT.jar:. simple.SimpleParallelTest
-javac -cp ../../target/jvstm-1.0-SNAPSHOT.jar:. simple/SimpleDisjointTest.java
-java -cp ../../target/jvstm-1.0-SNAPSHOT.jar:. simple.SimpleDisjointTest
-rm simple/*.class
